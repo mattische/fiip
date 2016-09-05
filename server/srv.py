f74eb886f78adce3a715a9ffd7fe99c881aedf5f
mattische <schertell@gmail.com>
2016-09-05 20:15:30 +0200

    database server code

--- server/srv.py
+++ server/srv.py
@@ -1,11 +1,38 @@
 from flask import Flask
+import sqlite3 as sql
+
+
+
+con = sql.connect('db.db')
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/showip/<ip>')
-def show_ip(ip):
+@app.route('/setip/<ip>')
+def set_ip(ip):
+    cur = con.cursor()
 
-    return 'Ip: %s' % ip
+    cur.execute("INSERT INTO Ips (ip, date) VALUES('" + ip + "','now')")
+    return "ok"
+
+
+@app.route('/showip/')
+def show_ip():
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Ips DESC LIMIT 1")
+    rows = cur.fetchall()
+
+
+    return 'Ip: %s' % rows
+
+
+def db_setup():
+    cur = con.cursor()    
+    cur.execute("CREATE TABLE IF NOT EXISTS Ips(id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date TEXT)")
+
+
+db_setup()
